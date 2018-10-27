4297a7f951d97394e96f55f9a62a7d7358d549d7
Kevin Riquena <riquena969@gmail.com>
2018-10-26 23:36:31 -0300

    Iniciado formulário de login

---
+++ web/application/views/login/login.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>eNove</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6 l4 offset-m3 offset-l4">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="row">
+                            <form class="col s12">
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix">account_circle</i>
+                                        <input id="usuario" type="text" class="validate">
+                                        <label for="usuario">Usuário</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="input-field col s12">
+                                        <i class="material-icons prefix">https</i>
+                                        <input id="senha" type="password" class="validate">
+                                        <label for="senha">Senha</label>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col s12 right-align">
+                                        <button type="button" class="waves-effect waves-light btn">Acessar <i class="material-icons right">chevron_right</i></button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</body>
+
+</html>
