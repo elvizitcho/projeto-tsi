e6b0c6d4b8156b548bca550ed611af5c1935a3aa
Rafael augusto <rafaelaro397@gmail.com>
2018-11-05 20:46:46 -0200

    CRUD de Dispositivo finalizado

---
+++ webservice/application/controllers/Dispositivo.php
@@ -0,0 +1,172 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dispositivo extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->load->model('Login_Model', 'loginModel');
+
+        $this->usuario = $this->loginModel->verificaPermissao($this->input->post('permissao'));
+
+        if ($this->usuario == null) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => 'Sem permissão'
+            )));
+        }
+    }
+
+    public function create() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('nome', 'Nome', 'required|min_length[2]|max_length[64]');
+        $this->form_validation->set_rules('comodo_id', 'Cômodo', 'required|is_natural');
+        $this->form_validation->set_rules('tipo', 'Tipo', 'required|in_list[1,2]');
+        $this->form_validation->set_rules('potencia', 'Potência', 'required|numeric');
+        $this->form_validation->set_rules('porta', 'Porta', 'required|is_natural');
+
+        if ($this->form_validation->run() == FALSE) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => validation_errors('<p class="red-text text-darken-2">', '</p>')
+            )));
+        }
+
+        $dispositivo = array(
+            'nome'      => $this->input->post('nome'),
+            'comodo_id' => $this->input->post('comodo_id'),
+            'tipo'      => $this->input->post('tipo'),
+            'potencia'  => $this->input->post('potencia'),
+            'porta'     => $this->input->post('porta'),
+            'status'    => 1
+        );
+
+        $this->load->model('Dispositivo_Model', 'model');
+
+        $idDispositivo = $this->model->create($dispositivo);
+
+        if ($idDispositivo == false) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => 'Falha ao salvar dispositivo no banco de dados'
+            )));
+        } else {
+            die(json_encode(array(
+                'sucesso'  => true,
+                'id'       => $idDispositivo
+            )));
+        }
+    }
+
+    public function update() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('id', 'ID', 'required|is_natural');
+        $this->form_validation->set_rules('nome', 'Nome', 'required|min_length[2]|max_length[64]');
+        $this->form_validation->set_rules('comodo_id', 'Cômodo', 'required|is_natural');
+        $this->form_validation->set_rules('tipo', 'Tipo', 'required|in_list[1,2]');
+        $this->form_validation->set_rules('potencia', 'Potência', 'required|numeric');
+        $this->form_validation->set_rules('porta', 'Porta', 'required|is_natural');
+
+        if ($this->form_validation->run() == FALSE) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => validation_errors('<p class="red-text text-darken-2">', '</p>')
+            )));
+        }
+
+        $dispositivo = array(
+            'nome'      => $this->input->post('nome'),
+            'comodo_id' => $this->input->post('comodo_id'),
+            'tipo'      => $this->input->post('tipo'),
+            'potencia'  => $this->input->post('potencia'),
+            'porta'     => $this->input->post('porta'),
+            'status'    => 1
+        );
+
+        $this->load->model('Dispositivo_Model', 'model');
+
+        if ($this->model->update($this->input->post('id'), $dispositivo) == false) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => 'Falha ao atualizar o dispositivo no banco de dados'
+            )));
+        } else {
+            die(json_encode(array(
+                'sucesso'  => true
+            )));
+        }
+    }
+
+    public function list() {
+        $this->load->model('Dispositivo_Model', 'model');
+
+        $page          = $this->input->post('page') == null ? 0 : $this->input->post('page');
+        $size          = $this->input->post('size') == null ? 10 : $this->input->post('size');
+        $filter        = $this->input->post('filter') == null ? '' : $this->input->post('filter');
+        $dispositivoId = $this->input->post('dispositivo');
+
+        die(json_encode(array(
+            'sucesso' => true,
+            'dados'   => $this->model->list($page, $size, $filter, $dispositivoId)
+        )));
+    }
+
+    public function detalhes() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('id', 'ID', 'required|is_natural');
+
+        if ($this->form_validation->run() == FALSE) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => validation_errors('<p class="red-text text-darken-2">', '</p>')
+            )));
+        }
+
+        $id = $this->input->post('id');
+
+        $this->load->model('Dispositivo_Model', 'model');
+
+        die(json_encode(array(
+            'sucesso' => true,
+            'dados'   => $this->model->detalhes($id)
+        )));
+    }
+
+    public function deletar() {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('id', 'ID', 'required|is_natural');
+
+        if ($this->form_validation->run() == FALSE) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => validation_errors('<p class="red-text text-darken-2">', '</p>')
+            )));
+        }
+
+        $comodo = array(
+            'status' => 0
+        );
+
+        $this->load->model('Dispositivo_Model', 'model');
+
+        if ($this->model->update($this->input->post('id'), $comodo) == false) {
+            die(json_encode(array(
+                'sucesso'  => false,
+                'mensagem' => 'Falha ao atualizar o dispositivo no banco de dados'
+            )));
+        } else {
+            die(json_encode(array(
+                'sucesso'  => true
+            )));
+        }
+    }
+
+    public function ligar() {
+        echo 'Em construção...';
+    }
+
+    public function desligar() {
+        echo 'Em construção...';
+    }
+}
---
+++ webservice/application/models/Dispositivo_Model.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dispositivo_Model extends CI_Model {
+
+    public function create($data) {
+        return $this->db->insert('dispositivos', $data) ? $this->db->insert_id() : false;
+    }
+
+    public function update($id, $data) {
+        return $this->db->where('id', $id)
+        				->update('dispositivos', $data);
+    }
+
+    public function list($page, $size, $filter, $comodo = null) {
+    	$this->db->select('dispositivos.id, dispositivos.nome, comodos.id as comodo_id, comodos.nome as comodo_nome')
+                        ->join('comodos', 'comodos.id = dispositivos.comodo_id')
+    					->where('comodos.status', 1)
+    					->where('dispositivos.status', 1)
+        				->group_start()
+    						->where('dispositivos.id', $filter)
+    						->or_like('dispositivos.nome', $filter)
+    						->or_like('comodos.nome', $filter)
+						->group_end();
+        
+        if ($comodo != null) {
+            $this->db->where('comodo_id', $comodo);
+        }
+
+    	return $this->db->from('dispositivos')
+    					->limit($size, $page * $size)
+    					->get()
+    					->result();
+    }
+
+    public function detalhes($id) {
+    	return $this->db->select('*')
+    					->where('status', 1)
+						->where('id', $id)
+    					->from('dispositivos')
+    					->get()
+    					->first_row();
+    }
+}
