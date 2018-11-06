f5b2e62bd642d020bcabbb25c102034121cd49b7
Kevin Riquena <riquena969@gmail.com>
2018-11-06 19:51:30 -0200

    Atualizado página inicial do projeto

--- webservice/application/config/config.php
+++ webservice/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.01/web/';
+$config['base_url'] = 'http://127.0.01/webservice/';
 
 /*
 |--------------------------------------------------------------------------
---
+++ webservice/application/controllers/Tsi.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tsi extends CI_Controller {
+
+    public function index() {
+        echo '<p style="text-align:center">Webservice do Grupo 03 - eNove</p>';
+        echo '<p style="text-align:center"><a href="https://github.com/riquena969/projeto-tsi" target="_blank">https://github.com/riquena969/projeto-tsi</a></p>';
+    }
+}
