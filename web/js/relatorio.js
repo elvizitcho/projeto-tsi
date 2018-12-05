77e4ac75d6d018ac46a2634c940a437eb9df3c4a
Kevin Riquena <riquena969@gmail.com>
2018-12-04 22:12:15 -0200

    Adicionado gráfico no relatório

--- web/js/relatorio.js
+++ web/js/relatorio.js
@@ -2,10 +2,15 @@ if (!logado()) {
     window.location = `${url}`;
 }
 
+var backgroundColor = [];
+var borderColor     = [];
+var data            = [];
+
 $(document).ready(function() {
     $('#inicio, #fim').val(dataAtual());
 
     relatorioList();
+    gerarGrafico();
 });
 
 function relatorioList() {
@@ -27,6 +32,12 @@ function relatorioList() {
                 return;
             }
 
+            backgroundColor = [];
+            borderColor     = [];
+            data            = [];
+            labels          = [];
+            let cor;
+
             $('#tabelaHistorico tbody').html('');
             $.each(retorno.dados, function(key, value) {
                 $('#tabelaHistorico tbody').append(`
@@ -36,10 +47,51 @@ function relatorioList() {
                         <td>${value.consumo}</td>
                         <td>R$ ${parseFloat(value.valor).toFixed(2)}</td>
                     </tr>`);
-            })
+
+                cor = randomColor();
+
+                labels.push(`${value.dispositivo_nome} (${value.comodo_nome})`);
+                data.push(parseFloat(value.consumo));
+                backgroundColor.push(cor);
+                borderColor.push(cor);
+            });
+
+            gerarGrafico();
         } catch (e) {
             toastr.danger("Falha ao se comunicar com o servidor para listar os dispositivos");
             console.error(e);
         }
     });
+}
+
+function gerarGrafico() {
+    var ctx = document.getElementById("myChart").getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Consumo em Kw/h do dispositivo',
+                data: data,
+                backgroundColor: backgroundColor,
+                borderColor: borderColor,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+function randomColor() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
